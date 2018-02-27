dc1fc3c889afe461f3d36b4b07a0e3826d2027cf
Cesar TL <cesartronlozai@convendia.com>
2018-02-27 20:42:45 +0000

    init

---
+++ src/main/java/com/ctl/hashcode/hascode2018/Hascode2018Application.java
@@ -0,0 +1,12 @@
+package com.ctl.hashcode.hascode2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hascode2018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hascode2018Application.class, args);
+	}
+}
